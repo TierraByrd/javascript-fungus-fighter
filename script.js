888d6e68c1b77b94176f183c2e6513926b96942d
Tierra Byrd <tierrabyrd1@gmail.com>
2024-05-06 22:07:13 -0500

    added function for each attack

--- script.js
+++ script.js
@@ -4,7 +4,10 @@
 // let fungusHP = 100;
 
 function onReady() {
-    console.log("Ready to go!")
+    console.log("Ready to go!")}
+
+
+    onReady()
     
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
@@ -12,13 +15,29 @@ function onReady() {
     
     // 🧠 Remember
     // define variables
-let attacksAP = 100;
-let enemyHP = 100;
+let myAP = 100;
+let fungusHP = 100;
 
     // - Handle events that ->
+    //start with function. Copy & paste name from 'index.html'
+    function arcaneScepterAttack(event){
+console.log('Arcane-Scepter Attack: ', arcaneScepterAttack());
+    }
+// make a function the same way with the remainding elements
+function entangleAttack(event){
+    console.log('Entangle Attack: ', entangleAttack());
+}
+
+    function dragonBladeAttack(event){
+        console.log('Dragon-Blade Attack: ', dragonBladeAttack());
+    }
+
+        function starFireAttack(event){
+            console.log('Star-Fire Attack: ', starFireAttack());
+        }
+
     // - Updates state which is ->
     // - Rendered to the DOM
-}
 
 
 onReady()
